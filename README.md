6a4943b64fedbd90b474f09d2a243fefe36dc042
Nguyen Viet Quang <v.QuangNV58@vinai.io>
2024-02-27 23:52:32 +0700

    add stuff

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ![](task.jfif)
+
+# Luồng Payment:
+- Màn hình checkout: bao gồm:
+    - Thông tin giỏ hàng: từng sản phẩm, số lượng từng cái, giá từng cái
+    - địa chỉ giao hàng: không có mapview, cho chọn thành phố, quận huyện, xã, địa chỉ cụ thể, số điện thoại người nhận
+    - chọn phương thức thanh toán: COD hoặc thanh toán bằng 
+- Màn hình chọn địa chỉ giao hàng
+- Màn hình chọn phương thức thanh toán:
+  - API thẻ sẽ do mình cung cấp, do mình tạo và check luôn
+  - Cho phép user tạo thẻ mới ở chỗ này
+  - Thêm tiền vào các thẻ có hai cách: từ tk này chuyển qua tk kia hoặc mình sẽ vào admin của backend
+- Màn hình success/failed
--- app/src/main/java/dev/vstd/shoppingcart/MainActivity.kt
+++ app/src/main/java/dev/vstd/shoppingcart/MainActivity.kt
@@ -1,14 +1,24 @@
 package dev.vstd.shoppingcart
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import dev.keego.musicplayer.R
+import dev.vstd.shoppingcart.ui.payment.PaymentActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            Intent(this@MainActivity, PaymentActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }

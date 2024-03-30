e0aaaca9dda14d27dfa24e6902fef56fdad5bbfc
Nguyen Viet Quang <v.QuangNV58@vinai.io>
2024-03-30 19:58:31 +0700

    on work login signup feature

--- app/src/main/java/dev/vstd/shoppingcart/data/APIErrorUtil.kt
+++ app/src/main/java/dev/vstd/shoppingcart/data/APIErrorUtil.kt
@@ -1,10 +1,10 @@
 package dev.vstd.shoppingcart.data
 
-import com.google.gson.Gson
 import retrofit2.Response
 
 object APIErrorUtil {
     fun getAPIError(response: Response<*>): APIError {
-        return Gson().fromJson(response.errorBody()!!.string(), APIError::class.java)
+        val message = response.errorBody()!!.string()
+        return APIError(response.code(), message)
     }
 }
--- app/src/main/java/dev/vstd/shoppingcart/data/remote/user/SignupResponse.kt
+++ app/src/main/java/dev/vstd/shoppingcart/data/remote/user/SignupResponse.kt
@@ -2,6 +2,4 @@ package dev.vstd.shoppingcart.data.remote.user
 
 data class SignupResponse(
     val message: String,
-    val token: String,
-    val username: String
 )
